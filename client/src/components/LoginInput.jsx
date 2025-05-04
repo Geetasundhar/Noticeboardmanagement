2212bff5b34bc5d5e204525fb94f9737f5612602
Sailash05 <sailashs2005@gmail.com>
2025-05-04 08:17:06 +0530

    Basic signup finished

---
+++ client/src/components/LoginInput.jsx
@@ -0,0 +1,15 @@
+// LoginInput.jsx
+const LoginInput = ({ type, placeHolder, value, onChange, name }) => {
+  return (
+    <input 
+      type={type} 
+      placeholder={placeHolder} 
+      value={value} 
+      onChange={onChange} 
+      name={name} 
+      className="w-full p-1 rounded-sm pl-3"
+    />
+  );
+}
+
+export default LoginInput;
---
+++ client/src/pages/login/Login.jsx
@@ -0,0 +1,10 @@
+import StudentLogin from "./StudentLogin";
+import StudentSignup from "./StudentSignup";
+
+const Login = () => {
+    return(
+        <StudentSignup />
+    );
+}
+
+export default Login;
